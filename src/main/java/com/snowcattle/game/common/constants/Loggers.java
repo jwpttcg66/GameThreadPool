041443f24364ffb62544a215f8870388c3079e5b
jiangwenping <jwpttcg66>
2017-04-06 11:03:12 +0800

    Initial commit

---
+++ src/main/java/com/snowcattle/game/common/constants/Loggers.java
@@ -0,0 +1,14 @@
+package com.snowcattle.game.common.constants;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 统一定义系统使用的slf4j的Logger
+ *
+ *
+ */
+public class Loggers {
+    /**thread相关的日志 */
+    public static final Logger threadLogger = LoggerFactory.getLogger("thread");
+}
---
+++ src/main/java/com/snowcattle/game/thread/policy/DiscardedPolicy.java
@@ -0,0 +1,32 @@
+package com.snowcattle.game.thread.policy;
+
+import com.snowcattle.game.common.constants.Loggers;
+import org.slf4j.Logger;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * Created by jwp on 2017/3/8.
+ */
+public class DiscardedPolicy extends ThreadPoolExecutor.DiscardOldestPolicy {
+    private static final Logger logger = Loggers.threadLogger;
+
+    private String threadName;
+
+    public DiscardedPolicy() {
+        this(null);
+    }
+
+    public DiscardedPolicy(String threadName) {
+        this.threadName = threadName;
+    }
+
+    public void rejectedExecution(Runnable runnable, ThreadPoolExecutor executor) {
+        if (threadName != null) {
+            logger.error("RPC Thread pool [{}] is exhausted, executor={}", threadName, executor.toString());
+        }
+
+        super.rejectedExecution(runnable, executor);
+    }
+}
+
